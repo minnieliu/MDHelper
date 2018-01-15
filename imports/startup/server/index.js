4f4ebb3e0e28bd7ec5db8b50d7a27e4a0211bb3b
minnieliu <minnieliu96@hotmail.com>
2018-01-14 18:04:39 -0800

    adding email url

---
+++ imports/startup/server/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by minnieliu on 2018-01-14.
+ */
+process.env.MAIL_URL = "smtps://postmaster@sandbox1551f43a07ce426e9c0b514db0e838ae.mailgun.org:263f40acf6fe249364ebc6d494d46614@smtp.mailgun.org:587"
--- server/main.js
+++ server/main.js
@@ -3,3 +3,8 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
 })
 ;
+
+
+//Meteor.methods({
+//    send_email: function
+//})
